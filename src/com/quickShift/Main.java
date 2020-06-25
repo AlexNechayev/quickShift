fdbbae5d9dde8ec4beb825799dd41b28c4c4dbce
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-06-26 00:48:46 +0300

    LoginController test was added

--- src/com/quickShift/Main.java
+++ src/com/quickShift/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
-        /*Model model = new Model();
-        Controller controller = new Controller(loginFrame,model);*/
+        Model model = new Model();
+        Controller controller = new Controller(loginFrame,model);
     }
 }
